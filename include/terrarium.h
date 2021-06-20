43666de67e6f18ea3c730a214474b9ba69dc445e
redrossa <adriano.raksi@gmail.com>
2021-06-20 00:55:40 -0500

    Implemented merge flush

--- include/terrarium.h
+++ include/terrarium.h
@@ -9,15 +9,35 @@
 
 typedef struct
 {
-    unsigned char ** rows;
+    unsigned char * data;
     size_t pixwidth;
     size_t pixheight;
 } terrarium_t;
 
+#define TERRARIUM_TILE_DIM 256
 #define TERRARIUM_PIXEL_SIZE 3
+#define TERRARIUM_PIXEL(terra, x, y) ((x + y * terra->pixwidth) * TERRARIUM_PIXEL_SIZE)
+#define TERRARIUM_PIXEL_TOP(terra, x, y) (TERRARIUM_PIXEL(terra, x, y) - terra->pixwidth * TERRARIUM_PIXEL_SIZE)
+#define TERRARIUM_PIXEL_BOT(terra, x, y) (TERRARIUM_PIXEL(terra, x, y) + terra->pixwidth * TERRARIUM_PIXEL_SIZE)
 
-size_t terrarium_tile_flush(tile_data_t *, va_list);
+terrarium_t * terrarium_init(size_t, size_t);
 
-size_t terrarium_heights(unsigned char *, size_t, terrarium_t *);
+void terrarium_free(terrarium_t *);
+
+size_t terrarium_tile_fflush(tile_data_t *, map_t *, va_list);
+
+size_t terrarium_map_fflush(tile_data_t *, map_t *, va_list);
+
+int terrarium_jigsaw(terrarium_t *, unsigned int, unsigned int, terrarium_t *);
+
+int terrarium_merge(terrarium_t *, terrarium_t **, size_t, size_t);
+
+terrarium_t * terrarium_init_decode(unsigned char *png_bytes, size_t size);
+
+int terrarium_get_pixel(terrarium_t *, int, int, unsigned char *);
+
+int terrarium_get_top_pixel(terrarium_t *, int, int, unsigned char *);
+
+int terrarium_get_bottom_pixel(terrarium_t *, int, int, unsigned char *);
 
 #endif //MAPTILE_TERRARIUM_H
--- src/terrarium.c
+++ src/terrarium.c
@@ -5,15 +5,37 @@
 #include "../include/terrarium.h"
 
 #include "../include/spng.h"
+#include "../include/lodepng.h"
 
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
 
-size_t terrarium_tile_flush(tile_data_t * td, va_list args)
+terrarium_t * terrarium_init(size_t pixw, size_t pixh)
+{
+    terrarium_t * t = malloc(sizeof(terrarium_t));
+    if (!t)
+        return NULL;
+    t->data = malloc(sizeof(unsigned char) * pixh * pixw * TERRARIUM_PIXEL_SIZE);
+    if (!t->data)
+    {
+        free(t);
+        return NULL;
+    }
+    t->pixwidth = pixw;
+    t->pixheight = pixh;
+    return t;
+}
+
+void terrarium_free(terrarium_t * t)
+{
+    free(t->data);
+    free(t);
+}
+
+size_t terrarium_tile_fflush(tile_data_t * td, map_t * map, va_list args)
 {
     char * dirname = va_arg(args, char *);
-
     char * fname = flush_tile_fname(dirname, td, "terrarium.png");
     if (!fname)
         return 0;
@@ -23,8 +45,8 @@ size_t terrarium_tile_flush(tile_data_t * td, va_list args)
     {
         free(fname);
         return 0;
-
     }
+
     size_t res = fwrite(td->data, sizeof(*td->data), td->size, fp);
 
     fclose(fp);
@@ -32,70 +54,150 @@ size_t terrarium_tile_flush(tile_data_t * td, va_list args)
     return res;
 }
 
-static size_t dispatch_error(spng_ctx * ctx, char * msg, int mem, ...)
+int terrarium_jigsaw(terrarium_t * dst, unsigned int xpix, unsigned int ypix, terrarium_t * src)
 {
-    fprintf(stderr, "%s\n", msg);
-    spng_ctx_free(ctx);
-    va_list args;
-    va_start(args, mem);
-    for (int i = 0; i < mem; i++)
-        free(va_arg(args, void *));
-    va_end(args);
-    return 0;
+    if (dst->pixwidth < xpix + src->pixwidth || dst->pixheight < ypix + src->pixheight)
+        return 0;
+    unsigned int dst_start = TERRARIUM_PIXEL(dst, xpix, ypix);
+    for (int i = 0; i < src->pixheight; i++)
+    {
+        unsigned int dst_row_start = dst_start + i * dst->pixwidth * TERRARIUM_PIXEL_SIZE;
+        unsigned int src_row_start = i * src->pixwidth * TERRARIUM_PIXEL_SIZE;
+        memcpy(&dst->data[dst_row_start], &src->data[src_row_start], src->pixwidth * TERRARIUM_PIXEL_SIZE);
+    }
+    return 1;
 }
 
-size_t terrarium_heights(unsigned char * png_bytes, size_t size, terrarium_t * terra)
+int terrarium_merge(terrarium_t * dst, terrarium_t ** srcs, size_t xcount, size_t ycount)
+{
+    size_t tile_count = xcount * ycount;
+    size_t tile_width = (srcs[0])->pixwidth;
+    size_t tile_height = (srcs[0])->pixheight;
+    for (int i = 0; i < tile_count; i++)
+    {
+        if (srcs[i]->pixwidth != tile_width || srcs[i]->pixheight != tile_height)
+            return 0;
+        unsigned int map_byte_x = i % xcount * tile_width;
+        unsigned int map_byte_y = i / xcount * tile_height;
+        if (!terrarium_jigsaw(dst, map_byte_x, map_byte_y, srcs[i]))
+            return 0;
+    }
+
+    return 1;
+}
+
+static void free_merger_holder(terrarium_t ** holder, int n)
+{
+    for (int i = 0; i < n; i++)
+        if (holder[i])
+            terrarium_free(holder[i]);
+    free(holder);
+}
+
+size_t terrarium_map_fflush(tile_data_t * td, map_t * map, va_list args)
+{
+    static int itc = 0;
+    static terrarium_t ** holder;
+
+    if (!itc++)
+    {
+        holder = calloc(map->tile_count, sizeof(terrarium_t *));
+        if (!holder)
+            return 0;
+    }
+
+    if (holder)
+    {
+        int current_tile_index = tile_toindex(td->info, map);
+        terrarium_t * t = terrarium_init_decode(td->data, td->size);
+        if (!t)
+        {
+            free_merger_holder(holder, map->tile_count);
+            return 0;
+        }
+        holder[current_tile_index] = t;
+
+        if (itc == map->tile_count)
+        {
+            itc = 0;
+            terrarium_t * merged = terrarium_init(map->xshape * TERRARIUM_TILE_DIM, map->yshape * TERRARIUM_TILE_DIM);
+            if (!merged)
+            {
+                free_merger_holder(holder, map->tile_count);
+                return 0;
+            }
+            if (!terrarium_merge(merged, holder, map->xshape, map->yshape))
+            {
+                free_merger_holder(holder, map->tile_count);
+                terrarium_free(merged);
+                return 0;
+            }
+
+            char * dirname = va_arg(args, char *);
+            char * fname = flush_map_fname(dirname, map, "terrarium.png");
+            if (!fname)
+            {
+                free_merger_holder(holder, map->tile_count);
+                terrarium_free(merged);
+                return 0;
+            }
+
+            lodepng_encode24_file(fname, merged->data, merged->pixwidth, merged->pixheight);
+        }
+    }
+
+    return td->size;
+}
+
+terrarium_t * terrarium_init_decode(unsigned char * png_bytes, size_t size)
 {
     /* Initialize spng context */
     spng_ctx * ctx = spng_ctx_new(0);
     if (!ctx)
-        return dispatch_error(ctx, "Cannot initialize context", 0);
+    {
+        return NULL;
+    }
 
     /* Set context to the buffer containing the terrarium data encoded in png */
     if (spng_set_png_buffer(ctx, png_bytes, size))
-        return dispatch_error(ctx, "Cannot set buffer", 0);
+    {
+        spng_ctx_free(ctx);
+        return NULL;
+    }
 
     /* Retrieve IHDR */
     struct spng_ihdr ihdr;
     if (spng_get_ihdr(ctx, &ihdr))
-        return dispatch_error(ctx, "Cannot retrieve IHDR", 0);
+    {
+        spng_ctx_free(ctx);
+        return NULL;
+    }
 
     /* Set the pixel pixwidth and pixheight */
-    terra->pixwidth = ihdr.width;
-    terra->pixheight = ihdr.height;
+    terrarium_t * t = terrarium_init(ihdr.width, ihdr.height);
+    if (!t)
+    {
+        spng_ctx_free(ctx);
+        return NULL;
+    }
 
     /* Get the size of the image bytes */
     size_t out_size;
     if (spng_decoded_image_size(ctx, SPNG_FMT_RGB8, &out_size))
-        return dispatch_error(ctx, "Cannot retrieve image size", 0);
-
-    /* Get the pixwidth of the image bytes */
-    size_t out_width = out_size / ihdr.height;
-
-    unsigned char * tmp = malloc(sizeof(unsigned char) * out_size);
-    if (!tmp)
-        return dispatch_error(ctx, "Cannot allocate memory", 0);
-    if(spng_decode_image(ctx, tmp, out_size, SPNG_FMT_RGB8, SPNG_FMT_RGB8))
-        return dispatch_error(ctx, "Cannot decode image", 1, (void *) tmp);
-
-    /* Allocate terrarium rows */
-    terra->rows = malloc(sizeof(unsigned char *) * ihdr.height);
-    if (!terra->rows)
-        return dispatch_error(ctx, "Cannot allocate memory", 1, (void *) tmp);
-
-    for (int i = 0; i < ihdr.height; i++)
     {
-        terra->rows[i] = malloc(sizeof(unsigned char) * out_width);
-        if (!terra->rows[i])
-        {
-            for (int j = 0; j < i; j++)
-                free(terra->rows[j]);
-            return dispatch_error(ctx, "Cannot allocate memory", 2, (void *) tmp, (void *) terra->rows);
-        }
-        memcpy(terra->rows[i], tmp + i * out_width, out_width);
+        terrarium_free(t);
+        spng_ctx_free(ctx);
+        return NULL;
+    }
+
+    if(spng_decode_image(ctx, t->data, out_size, SPNG_FMT_RGB8, SPNG_FMT_RGB8))
+    {
+        terrarium_free(t);
+        spng_ctx_free(ctx);
+        return NULL;
     }
 
     spng_ctx_free(ctx);
 
-    return out_size;
+    return t;
 }
