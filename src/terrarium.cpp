ee7fd0e94f9cc555d9a206515efc733bd65c8811
Reno Raksi <adriano.raksi@gmail.com>
2021-06-25 03:06:23 -0500

    Replaced sprintf with ostringstream

--- src/terrarium.cpp
+++ src/terrarium.cpp
@@ -4,16 +4,18 @@
 
 #include "../include/terrarium.h"
 
+#include <sstream>
+
 using namespace terrarium;
 using namespace maptile;
 
-#define TERRARIUM_ENDPOINT "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/%d/%d/%d.png"
+#define TERRARIUM_ENDPOINT "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/"
 
 transfer* builder::operator()(transfer::iterator i)
 {
     index_t id = *i;
     tile t = m.tile_from_index(id);
-    char url[256];
-    sprintf(url, TERRARIUM_ENDPOINT, t.zoom, t.x, t.y);
-    return new transfer(id, std::string(url));
+    std::ostringstream oss;
+    oss << TERRARIUM_ENDPOINT << "/" << t.zoom << "/" << t.x << "/" << t.y << ".png";
+    return new transfer(id, oss.str());
 }
